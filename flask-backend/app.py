da674ef92243639f7a99f4bf375487bc7bfdbf29
harshavar28 <saiharshavardhan742@gmail.com>
2025-09-09 10:40:30 +0530

    docker finish

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -11,7 +11,7 @@ app.config.from_object(Config)
 
 CORS(app)  # Enable CORS for all routes
 
-db.init_app(app)
+db.init_app(app)  
 
 # Create upload folder if not exists
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
